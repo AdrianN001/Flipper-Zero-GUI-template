6452a86ce9651b1f4546fec64979974017ed6126
Adrian <gogosadrian@outlook.com>
2024-03-22 18:15:42 +0100

    rename

--- scenes/scenes.h
+++ scenes/scenes.h
@@ -14,23 +14,23 @@ typedef enum {
   Scene_count
 } ApplicationScene;
 
-static void (*const diagnostic_scene_on_enter_handlers[])(void*) = {
+static void (*const template_scene_on_enter_handlers[])(void*) = {
     main_page_scene_on_enter,
     warning_page_scene_on_enter,
     connected_page_scene_on_enter};
 
-static bool (*const diagnostic_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
+static bool (*const template_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
     main_page_scene_on_event,
     warning_page_scene_on_event,
     connected_page_scene_on_event};
 
-static void (*const diagnostic_scene_on_exit_handlers[])(
+static void (*const template_scene_on_exit_handlers[])(
     void*) = {main_page_scene_on_exit, warning_page_scene_on_exit, connected_page_scene_on_exit};
 
 /** collection of all on_enter, on_event, on_exit handlers */
-static SceneManagerHandlers diagnostics_scene_event_handlers = {
+static SceneManagerHandlers templates_scene_event_handlers = {
 
-    .on_enter_handlers = diagnostic_scene_on_enter_handlers,
-    .on_event_handlers = diagnostic_scene_on_event_handlers,
-    .on_exit_handlers = diagnostic_scene_on_exit_handlers,
+    .on_enter_handlers = template_scene_on_enter_handlers,
+    .on_event_handlers = template_scene_on_event_handlers,
+    .on_exit_handlers = template_scene_on_exit_handlers,
     .scene_num = Scene_count};
